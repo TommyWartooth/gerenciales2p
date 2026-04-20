d330c6150fbf7212f896d909e641139b3e2df4ef
Marcel687 <marcelmurillo687@gmail.com>
2026-04-19 23:26:29 -0400

    Instalar dependencias backend y frontend, configurar alias Vite, agregar ruta temporal recepcionista

---
+++ back/routes/pedido.routes.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import {
+  getAllPedidos,
+  getPedidoById,
+  crearPedido,
+  actualizarPedidoCompleto,
+  eliminarPedido,
+  actualizarEstadoPedido,
+  getPedidosHistorial,
+} from "../controllers/pedido.controller.js";
+
+const router = Router();
+
+router.get("/", getAllPedidos);
+
+router.patch("/:nropedido/estado", actualizarEstadoPedido);
+
+export default router;
